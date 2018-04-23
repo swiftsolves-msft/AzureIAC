5f2ba4e64c0ac6c73610bb3d273ccf55b948f9f0
swiftsolves-msft <30509195+swiftsolves-msft@users.noreply.github.com>
2018-04-22 21:57:26 -0400

    Add files via upload

---
+++ scripts/2WEBLBavset.ps1
@@ -0,0 +1,118 @@
+﻿$cred = Get-Credential
+
+$start = Get-Date
+
+# Variables for common values
+$rgName='rgWEBVMCACEN'
+$location='canadacentral'
+
+
+# Create a resource group.
+New-AzureRmResourceGroup -Name $rgName -Location $location
+
+# Get existing virtual network.
+
+$vnet = Get-AzureRmVirtualNetwork -ResourceGroupName "rgNRSNetworking" -Name 'CACEN-VNET'
+
+# Create a public IP address.
+$publicIp = New-AzureRmPublicIpAddress -ResourceGroupName $rgName -Name 'EL4LB-PIP' `
+  -Location $location -AllocationMethod Dynamic
+
+# Create a front-end IP configuration for the website.
+$feip = New-AzureRmLoadBalancerFrontendIpConfig -Name 'FrontEndPool-WEB' -PublicIpAddress $publicIp
+
+# Create the back-end address pool.
+$bepool = New-AzureRmLoadBalancerBackendAddressPoolConfig -Name 'BackEndPool-WEB'
+
+# Creates a load balancer probe on port 80.
+$probe = New-AzureRmLoadBalancerProbeConfig -Name 'HealthProbe80' -Protocol Http -Port 80 `
+  -RequestPath / -IntervalInSeconds 360 -ProbeCount 5
+
+# Creates a load balancer rule for port 80.
+$rule = New-AzureRmLoadBalancerRuleConfig -Name 'LoadBalancerRuleWeb' -Protocol Tcp `
+  -Probe $probe -FrontendPort 80 -BackendPort 80 `
+  -FrontendIpConfiguration $feip -BackendAddressPool $bePool
+
+# Create two NAT rules for port 3389.
+$natrule1 = New-AzureRmLoadBalancerInboundNatRuleConfig -Name 'LoadBalancerRDP1' -FrontendIpConfiguration $feip `
+  -Protocol tcp -FrontendPort 4221 -BackendPort 3389
+
+$natrule2 = New-AzureRmLoadBalancerInboundNatRuleConfig -Name 'LoadBalancerRDP2' -FrontendIpConfiguration $feip `
+  -Protocol tcp -FrontendPort 4222 -BackendPort 3389
+
+# Create a load balancer.
+$lb = New-AzureRmLoadBalancer -ResourceGroupName $rgName -Name 'WEB-EL4LB' -Location $location `
+  -FrontendIpConfiguration $feip -BackendAddressPool $bepool `
+  -Probe $probe -LoadBalancingRule $rule -InboundNatRule $natrule1,$natrule2
+
+# Create a network security group rule for port 3389.
+$rule1 = New-AzureRmNetworkSecurityRuleConfig -Name 'NetworkSecurityGroupRuleRDP' -Description 'Allow RDP' `
+  -Access Allow -Protocol Tcp -Direction Inbound -Priority 1000 `
+  -SourceAddressPrefix Internet -SourcePortRange * `
+  -DestinationAddressPrefix * -DestinationPortRange 3389
+
+# Create a network security group rule for port 80.
+$rule2 = New-AzureRmNetworkSecurityRuleConfig -Name 'NetworkSecurityGroupRuleHTTP' -Description 'Allow HTTP' `
+  -Access Allow -Protocol Tcp -Direction Inbound -Priority 2000 `
+  -SourceAddressPrefix Internet -SourcePortRange * `
+  -DestinationAddressPrefix * -DestinationPortRange 80
+
+# Create a network security group
+$nsg = New-AzureRmNetworkSecurityGroup -ResourceGroupName $RgName -Location $location `
+-Name 'NetworkSecurityGroupWEBCACEN' -SecurityRules $rule1,$rule2
+
+# Create two virtual network cards and associate with public IP address and NSG.
+$nicVM1 = New-AzureRmNetworkInterface -ResourceGroupName $rgName -Location $location `
+  -Name 'Nic1' -LoadBalancerBackendAddressPool $bepool -NetworkSecurityGroup $nsg `
+  -LoadBalancerInboundNatRule $natrule1 -Subnet $vnet.Subnets[4]
+
+$nicVM2 = New-AzureRmNetworkInterface -ResourceGroupName $rgName -Location $location `
+  -Name 'Nic2' -LoadBalancerBackendAddressPool $bepool -NetworkSecurityGroup $nsg `
+  -LoadBalancerInboundNatRule $natrule2 -Subnet $vnet.Subnets[4]
+
+# Create an availability set.
+$as = New-AzureRmAvailabilitySet -ResourceGroupName $rgName -Location $location `
+  -Name 'WEB-AVSet' -Sku Aligned -PlatformFaultDomainCount 3 -PlatformUpdateDomainCount 3
+
+# Create two virtual machines.
+
+# ############## VM1 ###############
+
+# Create a virtual machine configuration
+$vmConfig = New-AzureRmVMConfig -VMName 'WEB01' -VMSize Standard_DS2_v2 -AvailabilitySetId $as.Id | `
+  Set-AzureRmVMOperatingSystem -Windows -ComputerName 'WEB01' -Credential $cred | `
+  Set-AzureRmVMSourceImage -PublisherName MicrosoftWindowsServer -Offer WindowsServer `
+  -Skus 2016-Datacenter -Version latest | Add-AzureRmVMNetworkInterface -Id $nicVM1.Id
+
+# Create a virtual machine
+$vm1 = New-AzureRmVM -ResourceGroupName $rgName -Location $location -VM $vmConfig
+
+# ############## VM2 ###############
+
+# Create a virtual machine configuration
+$vmConfig = New-AzureRmVMConfig -VMName 'WEB02' -VMSize Standard_DS2_v2 -AvailabilitySetId $as.Id | `
+  Set-AzureRmVMOperatingSystem -Windows -ComputerName 'WEB02' -Credential $cred | `
+  Set-AzureRmVMSourceImage -PublisherName MicrosoftWindowsServer -Offer WindowsServer `
+  -Skus 2016-Datacenter -Version latest | Add-AzureRmVMNetworkInterface -Id $nicVM2.Id
+
+# Create a virtual machine
+$vm2 = New-AzureRmVM -ResourceGroupName $rgName -Location $location -VM $vmConfig
+
+# Install IIS on VM1 via CSE
+#$PublicSettings = '{"commandToExecute":"powershell Add-WindowsFeature Web-Server"}'
+
+#Set-AzureRmVMExtension -ExtensionName "IIS" -ResourceGroupName $rgName -VMName $vm1.Name `
+#  -Publisher "Microsoft.Compute" -ExtensionType "CustomScriptExtension" -TypeHandlerVersion 1.4 `
+#  -SettingString $PublicSettings -Location $location
+
+# Install IIS on VM2 via DSC Push
+#$PublicSettings = '{"ModulesURL":"https://github.com/Azure/azure-quickstart-templates/raw/master/dsc-extension-iis-server-windows-vm/ContosoWebsite.ps1.zip", "configurationFunction": "ContosoWebsite.ps1\\ContosoWebsite", "Properties": {"MachineName": "WEB02"} }'
+
+#Set-AzureRmVMExtension -ExtensionName "DSC" -ResourceGroupName $rgName -VMName $vm2.Name `
+#  -Publisher "Microsoft.Powershell" -ExtensionType "DSC" -TypeHandlerVersion 2.19 `
+#  -SettingString $PublicSettings -Location $location
+
+$End = Get-Date
+
+Write-Host $start
+Write-Host $end
---
+++ scripts/CSEandDSC.ps1
@@ -0,0 +1,20 @@
+﻿Add-AzureRmAccount -identity
+
+# Call Azure Resource Manager to get the service principal ID for the VM's MSI. 
+$vmInfoPs = Get-AzureRMVM -ResourceGroupName "rgIAC" -Name "IaCLab"
+$spID = $vmInfoPs.Identity.PrincipalId
+echo "The MSI service principal ID is $spID"
+
+# Install IIS on VM1 via CSE
+$PublicSettings = '{"commandToExecute":"powershell Add-WindowsFeature Web-Server"}'
+
+Set-AzureRmVMExtension -ExtensionName "IIS" -ResourceGroupName $rgName -VMName $vm1.Name `
+  -Publisher "Microsoft.Compute" -ExtensionType "CustomScriptExtension" -TypeHandlerVersion 1.4 `
+  -SettingString $PublicSettings -Location $location
+
+# Install IIS on VM2 via DSC Push
+$PublicSettings = '{"ModulesURL":"https://github.com/Azure/azure-quickstart-templates/raw/master/dsc-extension-iis-server-windows-vm/ContosoWebsite.ps1.zip", "configurationFunction": "ContosoWebsite.ps1\\ContosoWebsite", "Properties": {"MachineName": "WEB02"} }'
+
+Set-AzureRmVMExtension -ExtensionName "DSC" -ResourceGroupName $rgName -VMName $vm2.Name `
+  -Publisher "Microsoft.Powershell" -ExtensionType "DSC" -TypeHandlerVersion 2.19 `
+  -SettingString $PublicSettings -Location $location
